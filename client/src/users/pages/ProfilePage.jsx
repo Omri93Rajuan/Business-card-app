2826cfcb54522c94fa2b3f06dc5b098561875ea7
Omri Rajuan <107933333+Omri93Rajuan@users.noreply.github.com>
2023-02-15 23:43:20 +0200

    profilePage

--- client/src/users/pages/ProfilePage.jsx
+++ client/src/users/pages/ProfilePage.jsx
@@ -1,25 +1,23 @@
-import React, {  useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import useUsers from "../hooks/useUsers";
 import { Container } from "@mui/system";
 import PageHeader from "../../components/PageHeader";
 import Spinner from "../../components/Spinner";
 import Error from "../../components/Error";
-import { Avatar, Box, Typography } from "@mui/material";
+import { Avatar, Box, Typography, Grid } from "@mui/material";
 import mapUserToModel from "../helpers/normalization/mapUserToModel";
-
 
 const ProfilePage = () => {
   const [userData, setUserData] = useState("");
   const { handleGetUser, value } = useUsers();
-  const { isLoading, error,user} = value;
+  const { isLoading, error, user } = value;
 
-useEffect(() => {
-   handleGetUser(user._id)
-.then(data => {
-   const mapUser = mapUserToModel(data)
-       setUserData(mapUser);
-   })
-   }, [handleGetUser, user._id]);
+  useEffect(() => {
+    handleGetUser(user._id).then((data) => {
+      const mapUser = mapUserToModel(data);
+      setUserData(mapUser);
+    });
+  }, [handleGetUser, user._id]);
 
   return (
     <Container>
@@ -29,21 +27,61 @@ useEffect(() => {
       />
       {isLoading && <Spinner />}
       {error && <Error errorMessage={error} />}
-      {userData != null && (<> <Box className = "center" flexDirection={"column"}><Avatar
-  alt="busienes card img"
-  src={userData.url}
-  sx={{ width: 200, height: 200 }}
-/>
-<Typography variant="h6" color="initial">{`Name: ${userData.first}`}</Typography>
-<Typography variant="h6" color="initial">{`Last : ${userData.last}`}</Typography>
-<Typography variant="h6" color="initial">{`Email : ${userData.email}`}</Typography>
-<Typography variant="h6" color="initial">{`Phone : ${userData.phone}`}</Typography>
-<Typography variant="h6" color="initial">{`Address : ${userData.street} ${userData.houseNumber}  ${userData.city}`}</Typography>
-<Typography variant="h6" color="initial">{`Created At : ${userData.createdAt}`}</Typography>
-
-
-</Box>
- </> )}
+      {userData != null && (
+        <>
+          <Grid
+            container
+            minHeight={180}
+            spacing={5}
+            mb={5}
+            mt={5}
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Grid item>
+              
+                <Typography
+                  variant="h4"
+                  color="initial"
+                >{`First name: ${userData.first}`}</Typography>
+                <Typography
+                  variant="h4"
+                  color="initial"
+                >{`Last name : ${userData.last}`}</Typography>
+                <Typography
+                  variant="h4"
+                  color="initial"
+                >{`Email : ${userData.email}`}</Typography>
+                <Typography
+                  variant="h4"
+                  color="initial"
+                >{`Phone : ${userData.phone}`}</Typography>
+                <Typography
+                  variant="h4"
+                  color="initial"
+                >{`Address : ${userData.street} ${userData.houseNumber}  ${userData.city}`}</Typography>
+                   <Typography
+                  variant="h4"
+                  color="initial"
+                >{`Buseness: ${user.isBusiness}`}</Typography>
+                     <Typography
+                  variant="h4"
+                  color="initial"
+                >{`Admin : ${user.isAdmin}`}</Typography>
+             
+            </Grid>
+            <Grid item spacing={0}>
+              
+                <Avatar
+                  alt="busienes card img"
+                  src={userData.url}
+                  sx={{ width: 350, height: 350 }}
+                />
+          
+            </Grid>
+          </Grid>
+        </>
+      )}
     </Container>
   );
 };
